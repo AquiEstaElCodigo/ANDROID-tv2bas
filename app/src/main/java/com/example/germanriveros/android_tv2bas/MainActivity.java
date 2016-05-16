79be93d59388cd331a34988aaa7ebf04ad120f23
Germán Riveros <germanriveros@MacBook-Pro-de-German.local>
2016-05-16 12:36:28 -0300

    5.2 capturar y procesar

--- app/src/main/java/com/example/germanriveros/android_tv2bas/MainActivity.java
+++ app/src/main/java/com/example/germanriveros/android_tv2bas/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.germanriveros.android_tv2bas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_cafe);
     }
+
+
+
+
+
+    public void mostrarAlerta(View v)
+    {
+
+        Toast toast = Toast.makeText(getApplicationContext(), "Has hecho click",Toast.LENGTH_LONG);
+
+        toast.show();
+    }
+
+
 }
